480704728a71d5325cd8fb23829bc63c8a3b7e2d
Kurt Bartholomew <siever246@gmail.com>
2015-07-05 12:49:53 -0700

    (feat) Completed iterative and recursive solutions to element insert list

--- problems/simple_recursion/element_insert_list.js
+++ problems/simple_recursion/element_insert_list.js
@@ -1,11 +1,19 @@
 /*
-	Augment every element in a list with a new value. The elemts are lists themselves.
+	Augment every element in a list with a new value. 
+  The elements are lists themselves.
 	
 	Example: [[],[3],[7]] , 5
 	Returns [[5],[3,5],[7,5]]
 
 */
 
-var recursiveElementInsert = function(arr, element){
-	// TODO : Finish this
+var iterativeElementInsert = function(arr, element) {
+  return arr.map(function(arrayElement){
+    return arrayElement.concat(element);
+  });
+};
+
+var recursiveElementInsert = function(arr, element) {
+	if(!arr.length) { return arr; }
+  return recursiveElementInsert(arr.slice(0,-1),element).concat([arr[arr.length-1].concat(element)]);
 };
