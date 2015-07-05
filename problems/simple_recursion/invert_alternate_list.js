44d75ce946f8956497c35c8b72d9ce6f788d5205
Kurt Bartholomew <siever246@gmail.com>
2015-07-05 10:15:02 -0700

    (feat) Solved inverted sign list with iterative and recursive solutions

--- problems/simple_recursion/invert_alternate_list.js
+++ problems/simple_recursion/invert_alternate_list.js
@@ -10,6 +10,23 @@
 
 */
 
+var iterativeListInvert = function(arr) {
+  return arr.map(function(element,index) {
+    if(index % 2 === 0) {
+      return element < 0 ? -element : element;
+    } else {
+      return element < 0 ? element : -element;
+    }
+  });
+};
+
 var recursiveListInvert = function(arr) {
-  // TODO : Finish this
+  if(!arr.length) { return arr; }
+  return arr.length % 2 === 0 ?
+    arr[arr.length-1] < 0 ? 
+      recursiveListInvert(arr.slice(0,-1)).concat(arr[arr.length-1])
+      : recursiveListInvert(arr.slice(0,-1)).concat(-arr[arr.length-1])
+    : arr[arr.length-1] < 0 ?
+      recursiveListInvert(arr.slice(0,-1)).concat(-arr[arr.length-1])
+      : recursiveListInvert(arr.slice(0,-1)).concat(arr[arr.length-1]);
 };
