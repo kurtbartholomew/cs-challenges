878fa9f7c10185d07aa4325dcb7c5d4b45869cb7
Kurt Bartholomew <siever246@gmail.com>
2015-07-13 09:56:28 -0700

    (feat) Added matrix rotate problem and test

---
+++ problems/matrix_rotate/matrix_rotate.js
@@ -0,0 +1,20 @@
+/*
+  Given a square matrix ( m x m ), rotate its values 90 degrees clockwise
+
+  Example: [[1,2,3,4],
+            [5,6,7,8],
+            [9,10,11,12],
+            [13,14,15,16]]
+  Returns: [[13,9,5,1],
+            [14,10,6,2],
+            [15,11,7,3],
+            [16,12,8,4]]
+
+  Constraints:
+    Space Complexity = O(1)
+
+*/
+
+var matrixRotate = function(matrix) {
+  // TODO : Finish this
+};
--- test/spec/main.spec.js
+++ test/spec/main.spec.js
@@ -474,3 +474,10 @@ describe('String Compression', function() {
     assert.equal(stringCompress('banana'),'banana');
   });
 });
+
+describe('Rotating a Matrix', function() {
+  it('should return a matrix rotated 90 degrees', function() {
+    assert.deepEqual(matrixRotate([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]),
+      [[13,9,5,1],[14,10,6,2],[15,11,7,3],[16,12,8,4]]);
+  });
+});
