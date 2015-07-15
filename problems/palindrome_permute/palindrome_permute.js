65a5ab3f2350367c935fb75d982af1e0dd1ebbc3
Kurt Bartholomew <siever246@gmail.com>
2015-07-15 14:13:00 -0700

    (feat) Added solution iterative and bitwise solution to palindrome permute

--- problems/palindrome_permute/palindrome_permute.js
+++ problems/palindrome_permute/palindrome_permute.js
@@ -10,8 +10,41 @@
   Example: "helicop"
   Returns false
 
+  Constraints: Do not permute every permutation
+
 */
 
 var palindromicPermuter = function(string) {
 
+  var stringContents = {};
+  var currentLetter = '';
+
+  for(var i = 0, max = string.length; i < max; i++ ) {
+    currentLetter = string[i];
+    if(stringContents[currentLetter]) { stringContents[currentLetter]++; }
+    else { stringContents[currentLetter] = 1; }
+  }
+
+  var oddCountLetters = 0;
+  for(var letter in stringContents){
+    if(stringContents[letter] % 2 !== 0) {
+      oddCountLetters++;
+    }
+    if(oddCountLetters > 1) { return false; }
+  }
+
+  return true;
 };
+
+// Alternate solution using a bit array (a single int)
+// var palindromicPermuter = function(string) {
+//   var bitStringContents = 0;
+//   var charVal = 0;
+
+//   for(var i = 0, max = string.length; i < max; i++ ) {
+//     charVal = string.charCodeAt(i) - 96;
+//     bitStringContents ^= (1 << charVal);
+//   }
+
+//   return ((bitStringContents - 1) & bitStringContents) === 0;
+// };
