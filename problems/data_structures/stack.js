09c558c640af96d2e7887fcea6104e46b25a67a5
Kurt Bartholomew <siever246@gmail.com>
2015-07-19 12:29:09 -0700

    (feat) Added array and linked list solutions for stack problem

--- problems/data_structures/stack.js
+++ problems/data_structures/stack.js
@@ -21,25 +21,80 @@
 */
 
 function Stack() {
-  // TODO : Finish this
+  this.storage = [];
+  this.objectSize = 0;
 }
 
 Stack.prototype.size = function(){
-  // TODO : Finish this
+  return this.objectSize;
 };
 
 Stack.prototype.push = function(value){
-  // TODO : Finish this
+  this.objectSize++;
+  this.storage[this.objectSize] = value;
 };
 
 Stack.prototype.pop = function(){
-  // TODO : Finish this
+  if(this.objectSize === 0) { return null; }
+  var poppedVal = this.storage[this.objectSize];
+  delete this.storage[this.objectSize];
+  this.objectSize--;
+  return poppedVal;
 };
 
 Stack.prototype.peek = function(){
-  // TODO : Finish this
+  var peekedVal = this.storage[this.objectSize];
+  this.objectSize--;
+  return peekedVal;
 };
 
 Stack.prototype.isEmpty = function(){
-  // TODO : Finish this
+  if(!this.objectSize) { return true; }
+  return false;
 };
+
+// ========== Alternate Implementation ( Singly Linked List) ===========
+
+// function Stack() {
+//   this.top = null;
+// }
+
+// function StackNode(value) {
+//   this.value = value;
+//   this.next = null;
+// }
+
+// Stack.prototype.size = function(){
+//   if(this.top === null) { return 0; }
+//   var currentNode = this.top;
+//   var objectSize = 1;
+//   while(currentNode) {
+//     objectSize++;
+//     currentNode = currentNode.next;
+//   }
+//   return objectSize;
+// };
+
+// Stack.prototype.push = function(value){
+//   var newNode = new StackNode(value);
+//   if(this.top) { this.top.next = newNode; }
+//   this.top = newNode;
+// };
+
+// Stack.prototype.pop = function(){
+//   if(this.top === null) { return null; }
+//   var poppedVal = this.top.value;
+//   this.top = this.top.next;
+//   return poppedVal;
+// };
+
+// Stack.prototype.peek = function(){
+//   if(this.top === null) { return null; }
+//   var peekedVal = this.top.value;
+//   return peekedVal;
+// };
+
+// Stack.prototype.isEmpty = function(){
+//   if(this.top === null) { return true; }
+//   return false;
+// };
