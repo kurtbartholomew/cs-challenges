e93f9c23c62d36486a5257baca4b2c2844f26af5
Kurt Bartholomew <siever246@gmail.com>
2015-07-20 11:45:37 -0700

    (feat) Finished a solution for matrix rotate

--- problems/matrix_rotate/matrix_rotate.js
+++ problems/matrix_rotate/matrix_rotate.js
@@ -1,20 +1,42 @@
 /*
   Given a square matrix ( m x m ), rotate its values 90 degrees clockwise
 
-  Example: [[1,2,3,4],
-            [5,6,7,8],
-            [9,10,11,12],
-            [13,14,15,16]]
-  Returns: [[13,9,5,1],
-            [14,10,6,2],
-            [15,11,7,3],
-            [16,12,8,4]]
+  Example: [[1,  2,  3,  4 ],
+            [5,  6,  7,  8 ],
+            [9,  10, 11, 12],
+            [13, 14, 15, 16]]
+  Returns: [[13, 9,  5,  1],
+            [14, 10, 6,  2],
+            [15, 11, 7,  3],
+            [16, 12, 8,  4]]
 
   Constraints:
-    Space Complexity = O(1)
+    Do not create another array.
 
 */
 
 var matrixRotate = function(matrix) {
-  // TODO : Finish this
+  var numberOfLayers = matrix.length / 2;
+  var temp = 0;
+  var arrLength = matrix.length-1;
+  var end;
+
+  for(var layer = 0; layer < numberOfLayers; layer++) {
+    end = arrLength - layer;
+    for(var j = layer; j < end; j++) {
+      temp = matrix[layer][j];
+
+      // Left to Top
+      matrix[layer][j] = matrix[arrLength-j][layer];
+      // Bottom to Left
+      matrix[arrLength-j][layer] = matrix[end][arrLength-j];
+      // Right to Bottom
+      matrix[end][arrLength-j] = matrix[j][end];
+      // Top to Right
+      matrix[j][end] = temp;
+      console.log(matrix);
+      console.log('layer',layer,'j',j);
+    }
+  }
+  return matrix;
 };
