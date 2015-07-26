fa8a8a4df300c0149ba2f1f0c14b87f241200e8d
Kurt Bartholomew <siever246@gmail.com>
2015-07-26 11:17:47 -0700

    (feat) Added a solution to linked list palindrome solution

--- problems/linked_palindrome/linked_palindrome.js
+++ problems/linked_palindrome/linked_palindrome.js
@@ -11,5 +11,27 @@
 */
 
 var linkedListPalindromeFinder = function(linkedList) {
+  if(linkedList.head === null) { return null; }
+  var fastPointer = linkedList.head;
+  var slowPointer = linkedList.head;
+  var stacky = new Stack();
 
+  while(fastPointer !== null && fastPointer.next !== null){
+    stacky.push(slowPointer.value);
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+
+  if(fastPointer !== null) {
+    slowPointer = slowPointer.next;
+  }
+
+  while(slowPointer !== null){
+    if(slowPointer.value !== stacky.pop()){
+      return false;
+    }
+    slowPointer = slowPointer.next;
+  }
+
+  return true;
 };
