110c399115306fc92ec980d226190cbb942fd075
Kurt Bartholomew <siever246@gmail.com>
2015-07-28 18:29:18 -0700

    (feat) Added solution for multistack collection.

--- problems/multistack_collection/multistack_collection.js
+++ problems/multistack_collection/multistack_collection.js
@@ -6,13 +6,32 @@
 */
 
 var multiStackCollection = function(stackSize) {
-  // TODO : Finish this
+  this._storage = [];
+  this._storage.push(new Stack());
+  this.currentStack = 0;
+  this.sizeConstraint = stackSize;
 };
 
 multiStackCollection.prototype.push = function(value){
-  
+  if(this._storage[this.currentStack].objectSize === this.sizeConstraint){
+    this.currentStack++;
+    this._storage[this.currentStack] = new Stack();
+  }
+  this._storage[this.currentStack].push(value);
 };
 
 multiStackCollection.prototype.pop = function(){
-  
+  if(this._storage[this.currentStack].objectSize === 0 && this.currentStack === 0) {
+    return null;
+  } else if(this._storage[this.currentStack].objectSize === 0){
+    this.currentStack--;
+  }
+  return this._storage[this.currentStack].pop();
+};
+
+multiStackCollection.prototype.popAt = function(stackIndex){
+  if(this.currentStack < stackIndex || this._storage[stackIndex].objectSize === 0){
+    return null;
+  }
+  return this._storage[stackIndex-1].pop();
 };
