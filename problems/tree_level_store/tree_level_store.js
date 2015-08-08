bd36f9b65ea3b5c54c0228f89a50f69291e2aaf8
Kurt Bartholomew <siever246@gmail.com>
2015-08-08 10:21:34 -0700

    (feat) Added a solution for tree level store.

--- problems/tree_level_store/tree_level_store.js
+++ problems/tree_level_store/tree_level_store.js
@@ -2,7 +2,7 @@
   Given a binary tree with references to left and right children,
   return an array of linked lists containing the nodes at each level
   of the tree.
-  Note: Please use the linked list structure you created previously.)
+  (Note: Please use the linked list structure you created previously.)
 
   Example:     (5)
               /   \
@@ -14,5 +14,18 @@
 */
 
 var treeLevelStore = function(rootTreeNode){
-  // TODO : Finish this
+  if(!rootTreeNode) { throw new Error("You must provide a binary tree."); }
+  var result = [];
+  traverseLevels(rootTreeNode,0,result);
+  return result;
+};
+
+var traverseLevels = function(currentNode,depth,depthArray){
+  if(currentNode === null) { return ;}
+  if(currentNode.data !== null){
+    if(!depthArray[depth]) { depthArray[depth] = new LinkedList(); }
+    depthArray[depth].addToTail(currentNode.data);
+  }
+  traverseLevels(currentNode.left,depth+1,depthArray);
+  traverseLevels(currentNode.right,depth+1,depthArray);
 };
