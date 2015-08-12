03e00cfb19f220a92110f222471a91df6ec32812
Kurt Bartholomew <siever246@gmail.com>
2015-08-11 21:33:59 -0700

    (feat) Added least recently used cache problem

---
+++ problems/lru_cache/lru_cache.js
@@ -0,0 +1,14 @@
+/*
+  Create a least recently used cache. The cache should hold references to
+  the objects that it stores with a key value pair. The cache has a size
+  limit and once the limit is reached, the least used object is removed from
+  the cache.
+
+  Example:
+  Returns
+
+*/
+
+var lruCache = function(){
+  // TODO : Finish this
+};
